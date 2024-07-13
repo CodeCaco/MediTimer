22fa908420dc30eb6c4df188c91e7d3e5750026e
CodeCaco <cacoleitao@gmail.com>
2024-07-13 22:39:43 +0100

    V0-1 Init Swift Project

---
+++ MediTimer/MediTimer/MediTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MediTimerApp.swift
+//  MediTimer
+//
+//  Created by Vasco Leitão on 13/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct MediTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
